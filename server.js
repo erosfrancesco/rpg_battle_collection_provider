851153b936aea9e1908d1c5c78471d9270d1f838
eros francesco reale <eros.francesco.francesco@gmail.com>
2019-05-09 13:50:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,34 +3,39 @@
 /*********************************************************************/
 process.chdir(__dirname);
 
-
+require('dotenv').config();
 const express = require('express');
 const config = require('./config');
 const app = express();
 
-const assets = require('./routings/assets.js');
+const assets = require('./routings/assets');
 app.use('/assets', assets);
 
-const groups = require('./routings/groups.js');
+const groups = require('./routings/groups');
 app.use('/groups', groups);
 
-const categories = require('./routings/categories.js');
+const categories = require('./routings/categories');
 app.use('/categories', categories);
 
-
+/*
 //test
 app.get("/", async (req, res) => {
 	await createTestUser("piipo user");
 	res.json( await models.User.findByLogin("piipo user") )
 });
+/**/
 
 app.get("/upload", async (req, res) => res.sendFile(__dirname + "/testUpload.html") );
 
 
 
+/*
 const { connectDb, models } = require('./src/models');
 
 const createTestUser = async username => await models.User({username}).save();
+/**/
 
 const serverInitResponseHandler = error => console[error ? `error` : `log`](error ||  `server listening on port ${process.env.PORT}`); 
-connectDb().then(() => app.listen(process.env.PORT, serverInitResponseHandler) );
+const db = require("./db");
+db.then(() => app.listen(process.env.PORT, serverInitResponseHandler) );
+
