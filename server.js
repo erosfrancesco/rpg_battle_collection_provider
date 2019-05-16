6db8e348a8dcb5ac62241bcb5e5f60ad4c1c5e8f
eros francesco reale <eros.francesco.francesco@gmail.com>
2019-05-16 15:31:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,12 @@
 /* PROCESS */
 /*********************************************************************/
 const app = require('express')();
+const cors = require('cors')
 
 process.env.PORT = process.env.PORT || 80;
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 // routes for categories
 const categoryRouting = require("./routings");
