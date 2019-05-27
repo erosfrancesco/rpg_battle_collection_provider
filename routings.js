45bf2b02f639ff21d6d87fc0b6d703648c6a1442
eros francesco reale <eros.francesco.francesco@gmail.com>
2019-05-27 15:52:49 +0200

    Update routings.js
    added an import route

--- routings.js
+++ routings.js
@@ -106,4 +106,20 @@ router.route("/:category/:id")
 //
 
 
+// import
+router.post("/import/:category/", async (req, res) => {
+	const {category} = req.params;
+	const items = req.body;
+	const results = []
+
+	for (let item of items) {
+		const itemToBeSaved = new models[category](item);
+		const saved = await itemToBeSaved.save().catch(err => console.error(err))
+		results.push(saved)
+	}
+
+	res.json(results);
+})
+
+//
 module.exports = router;
