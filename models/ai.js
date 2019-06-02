61dd5f2bfffcdce1bc64f0eb1db4c522a8696f8a
eros francesco reale <eros.francesco.francesco@gmail.com>
2019-06-02 14:16:09 +0200

    Update ai.js

--- models/ai.js
+++ models/ai.js
@@ -11,14 +11,10 @@ const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
 	label: String,
 	properties: {
-		waterfall: [{
-			params: { type: String, default: "scene, actor, callback" },
-			body: String
-		}],
-		body: [String],
-		params: [{
+		body: String,
+		params: {
 			type: String, default: "scene, actor, callback"
-		}]
+		}
 	}
 });
 
