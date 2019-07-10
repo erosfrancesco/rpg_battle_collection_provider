eed6da71447d2f76d2ae4711b8c5540caec0ba53
Eros Reale <eros.francesco.francesco@gmail.com>
2019-07-10 17:48:46 +0200

    fix

--- db.js
+++ db.js
@@ -8,11 +8,12 @@ mongoose.set('useFindAndModify', false);
 
 //oldone: mongodb+srv://jester:Bondrewd717@rpgbattle-eztzq.mongodb.net/main?retryWrites=true
 
-process.env.MONGODB_USER = process.env.MONGODB_USER || "jester"//"madhatter";
-process.env.MONGODB_PASS = process.env.MONGODB_PASS || "Bondrewd717"//"Z8MfHgBd76RGvGvk";
-process.env.MONGODB_URL  = process.env.MONGODB_URL  || "@rpgbattle-eztzq.mongodb.net/main?retryWrites=true";
+//process.env.MONGODB_USER = process.env.MONGODB_USER || "jester"//"madhatter";
+//process.env.MONGODB_PASS = process.env.MONGODB_PASS || "Bondrewd717"//"Z8MfHgBd76RGvGvk";
+process.env.MONGODB_URL  = process.env.MONGODB_URL  || "mongodb+srv://jester:Bondrewd717@rpgbattle-eztzq.mongodb.net/main?retryWrites=true"
+//"@rpgbattle-eztzq.mongodb.net/main?retryWrites=true";
 
-const url = "mongodb+srv://" + process.env.MONGODB_USER  + ":" + process.env.MONGODB_PASS + process.env.MONGODB_URL;
+//const url = "mongodb+srv://" + process.env.MONGODB_USER  + ":" + process.env.MONGODB_PASS + process.env.MONGODB_URL;
 
 
 // If the Node process ends, close the Mongoose connection
@@ -24,7 +25,7 @@ process.on('SIGINT', gracefulExit).on('SIGTERM', gracefulExit);
 
 
 // Export as connection function
-module.exports = callback => mongoose.connect(url).then(
+module.exports = callback => mongoose.connect(process.env.MONGODB_URL).then(
 	res => { 
 		console.log("connected to db");
 		callback(null, res);
