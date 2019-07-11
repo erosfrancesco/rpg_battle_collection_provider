568a2ea3fa19042dc299b1e21b281e7e7300f05d
Eros Reale <eros.francesco.francesco@gmail.com>
2019-07-11 16:32:52 +0200

    ai fix

--- models/ai.js
+++ models/ai.js
@@ -9,19 +9,24 @@ const {Resource, EncodedFunction} = require("./utils");
 	}
 }
 */
-const schema = new mongoose.Schema({
-	label: String,
-	groups: [{
-		type: mongoose.Schema.Types.ObjectId, 
-		ref: 'groups'
-	}],
-	properties: {
-		body: String,
-		params: {
-			type: String, default: "scene, actor, callback"
-		}
-	}
-});
+const schema = Resource(
+	EncodedFunction("scene, options, callback", "callback();")
+);
+
+
+// {
+// 	label: String,
+// 	groups: [{
+// 		type: mongoose.Schema.Types.ObjectId, 
+// 		ref: 'groups'
+// 	}],
+// 	properties: {
+// 		body: String,
+// 		params: {
+// 			type: String, default: "scene, actor, callback"
+// 		}
+// 	}
+// });
 
 
 
